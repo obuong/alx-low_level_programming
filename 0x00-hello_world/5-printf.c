fe49fcc3f164eaca54a238c3efa85a803bcca21f
Esau Obuong <iobuong2021@gmail.com>
2022-09-09 01:02:45 -0700

    Q5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,25 +4,12 @@
 * main - print the string in the print dunction
 * 
 * Description: using the main function
-* the program prints "with proper grammar, but the outcome is a piece of art,
-* Return: 0
+* the program prints "with proper grammar, but the outcome is a piece of art,* Return: 0
 */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
-while (text[c] != '\0')
-{
-	if (!(text[c] == ' ' && text[c+1] == ' ')) {
-		blank[d] = text[c];
-		d++;
-	}
-	c++;
-	
 }
 
-blank[d] = '\0';
-printf("Text after removing blanks\n%s\n", blank);
-return 0;
 
