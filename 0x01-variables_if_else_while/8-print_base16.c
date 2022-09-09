d4073592f9249b226c7e29f1306a2c5f2a839ec9
Esau Obuong <iobuong2021@gmail.com>
2022-09-10 00:07:30 +0300

    Q8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * maain - print if the number is positive, zero, or negative
+ *
+ * Description: using the main functio
+ * this program prints "Programming is positive, zero or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c<= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
