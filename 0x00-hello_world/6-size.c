0651b93be9ced88f3a15ad5f55b97722ed60f2cc
Esau Obuong <iobuong2021@gmail.com>
2022-09-09 02:26:31 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - print the string in the put function
  *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
- * Retun: 0;
+ * Retun: 0
  */
-int main (void)
+int main(void)
 {	char c;
 	int i;
 	long li;
