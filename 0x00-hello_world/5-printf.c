616378223e7293e4edb53de0c2a68b1834930a84
Esau Obuong <iobuong2021@gmail.com>
2022-09-08 23:28:31 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+* main - print the string in the print dunction
+* 
+* Description: using the main function
+* the program prints "with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-
-	printf("size of a char: %lu byte(s)\n", (unasigned long)sizeof(d));
-	printf("size of an int: %lu byte(s)\n", (unasigned long)sizeof(a));
-	printf("size of a long int: %lu byte(s)\n", (unasigned long)sizeof(b));
-	printf("size of a long long int: %lu (byte(s)\n", (unasigned long)sizeof(d));
-		printf("size of a float: %lu byte(s)\n", (unasigned long)sizeof(f));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-
