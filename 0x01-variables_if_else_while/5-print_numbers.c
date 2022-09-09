2c59fc048e9dbe65d7aeb36ee3ae6f571a02acca
Esau Obuong <iobuong2021@gmail.com>
2022-09-09 23:45:07 +0300

    Q5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print is the numbber is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n);
+	return (0);
+}
+
