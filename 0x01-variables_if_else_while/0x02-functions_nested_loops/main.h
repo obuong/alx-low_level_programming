81233690eeb2228d624a5942ef330b45bea28c38
Esau Obuong <iobuong2021@gmail.com>
2022-09-14 00:16:50 +0300

    main

--- 0x01-variables_if_else_while/0x02-functions_nested_loops/main.h
+++ 0x01-variables_if_else_while/0x02-functions_nested_loops/main.h
@@ -1,12 +1,11 @@
-#ifndef HOLBERTON_H_INCLUDED 
+#ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-int_putchar(char);
-void print_alphabet(void)
-void print[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[);[B[B[D[D[D[D[A[D[D[D[D[D[D[D[D[D[D[D[D_alphabet_x10(void);
-int_islower(int c);
-int[A [B[A _is[D[D[3~[[C[C[C[_islower(int c);
-[C[C[C[C[C[C[C[C[C[C[C_isalpha(int c);
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -17,3 +16,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
+#endif
