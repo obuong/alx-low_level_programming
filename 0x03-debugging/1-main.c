2d76a3f913f9032227bd818373e6457f7b1a0bfe
Esau Obuong <iobuong2021@gmail.com>
2022-09-15 14:57:00 +0300

    Q1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,16 +8,17 @@
 int main(void)
 {
 int i;
+
 print("Infinite loop incoming :(\n");
 
 i = 0;
 /**
- * while (1 <10)
+ * while (1 < 10)
  * {
  * putchar(i);
  * }
  */
-printf("Infinite loop avoided! \\\o/n");
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
