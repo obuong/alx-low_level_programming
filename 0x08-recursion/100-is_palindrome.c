9cd369d6ca5bd59f27327ee87bae09c4599221fc
Esau Obuong <iobuong2021@gmail.com>
2022-09-28 21:53:57 -0700

    Q5

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,24 +7,16 @@ int _strlen_recursion(char *s);
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to reverse
- * 
- * Return: 1 if it is, 0 it's not
  *
- *     
+ * Return: 1 if it is, 0 it's not 
  */ 
 int is_palindrome(char *s)
 
 {
-
 	 if (*s == 0)
-
-		   return (1);
-
-	  return (check_pal(s, 0, _strlen_recursion(s)));
-
+	 return (1);
+	 return (check_pal(s, 0, _strlen_recursion(s)));
 }
-
-
 
 /**
  *
@@ -41,16 +33,10 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 
 {
-
 	 if (*s == '\0')
-
-		   return (0);
-
-	  return (1 + _strlen_recursion(s + 1));
-
+	 return (0);
+	 return (1 + _strlen_recursion(s + 1));
 }
-
-
 
 /**
  *
@@ -68,15 +54,9 @@ int _strlen_recursion(char *s)
 int check_pal(char *s, int i, int len)
 
 {
-
 	 if (*(s + i) != *(s + len - 1))
-
-		   return (0);
-
-	  if (i >= len)
-
-		    return (1);
-
-	   return (check_pal(s, i + 1, len - 1));
-
+	 return (0);
+	 if (i >= len)
+	return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
