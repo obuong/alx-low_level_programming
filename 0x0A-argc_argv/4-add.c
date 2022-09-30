a80f1ff2a168a5160198b96148493d0afa34fc1c
Esau Obuong <iobuong2021@gmail.com>
2022-09-30 23:25:35 +0300

    Q4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - A program that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num, result = 0, i;
+
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i]; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
