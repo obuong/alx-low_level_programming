da838ab71ecb949321f7da5068dab774388c0fe1
Esau Obuong <iobuong2021@gmail.com>
2022-10-04 12:10:23 +0300

    Q0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * @c: char to assign
  * Return: pointer to array or NULL if it fails
  */
-char *create_array(unasigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unasigned int i;
