75fc0b1de226f95008ccde9bb89a98afb31dd18c
obuong <iobuong@gmail.com>
2023-04-12 12:18:49 +0300

    executable

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,8 @@
 #define SEARCH_ALGOS_H
 #include <stdio.h>
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 #endif
